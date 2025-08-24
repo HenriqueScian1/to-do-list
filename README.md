dc5ba738158282474eea199138b2d31ae897e634
Henrique Sciani <henriquesciani@gmail.com>
2025-08-24 12:05:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# to-do-list
+Projeto para entrega na aula do dia 25/08/2025 da aula da Koru com parceria ao desenvolve do grupo boticario
---
+++ todo/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm';
+
+import "./App.css"
+
+  function App() {
+    const [todos, setTodos] = useState([
+      {
+        id: 1,
+        text: 'Fazer funcionalidade no sistema',
+        category: 'Trabalho',
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        text: 'Estudar React',
+        category: 'Estudos',
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        text: 'Ir ao supermercado',
+        category: 'Pessoal',
+        isCompleted: false,
+      },
+    ]);
+
+    const addTodo = (text, category) =>{ 
+    const newTodos = [...todos, {
+      id: Math.random() *10000,
+      text, 
+      category,
+      isCompleted: false
+    }]
+
+    setTodos(newTodos);
+  };
+
+    return <div className='app'>
+      <h1>Lista de tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>;
+
+}
+
+export default App

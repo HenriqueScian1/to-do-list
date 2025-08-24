0c5d9e4c8fe07f36a0d8f72e3339d004785e3bba
Henrique Sciani <henriquesciani@gmail.com>
2025-08-24 20:10:03 -0300

    Agenda Completa
    Agenda Completada - Adiciona CSS a vontade

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 
-import Todo from './components/Todo'
+import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 import "./App.css"
 
@@ -27,6 +29,11 @@ import "./App.css"
       },
     ]);
 
+    const [search, setSearch] = useState('');
+
+    const [filter, setFilter] = useState('All');
+    const [sort, setSort] = useState('Asc');
+
     const addTodo = (text, category) =>{ 
     const newTodos = [...todos, {
       id: Math.random() *10000,
@@ -38,11 +45,29 @@ import "./App.css"
     setTodos(newTodos);
   };
 
-    return <div className='app'>
+const removeTodo = (id) => {
+  const newTodos = [...todos]
+  const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+  setTodos(filteredTodos);
+};
+
+const completeTodo = (id) => {
+  const newTodos = [...todos];
+  newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+  setTodos(newTodos);
+};
+
+    return <div className='app'> 
       <h1>Lista de tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className='todo-list'>
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
+        {todos
+        .filter((todo) => filter === 'All' ? true : filter === 'Completed' ? todo.isCompleted : !todo.isCompleted)
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+        .map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
         ))}
       </div>
       <TodoForm addTodo={addTodo} />
--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
     return (
-         <div className="todo">
+         <div className="todo" style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
           <div className="content">
           <p>{todo.text}</p>
           <p className="category">({todo.category})</p>
           </div>
           <div>
-            <button className='complete'>Concluir</button>
-            <button className='remove'>X</button>
+            <button className='complete' onClick={() => completeTodo(todo.id)}>Concluir</button>
+            <button className='remove' onClick={() => removeTodo(todo.id)}>X</button>
           </div>
           </div>    
           );
 }
 
-export default Todo
+export default Todo 
